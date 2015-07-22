7986bedf71f1857691435f0bf72ce3c84cc4e40c
raaj-obuli <raaj-obuli@users.noreply.github.com>
2015-07-22 14:20:55 +0530

    temp

--- admin/achiever/achieverlist-view.php
+++ admin/achiever/achieverlist-view.php
@@ -1,3 +1,7 @@
+Will be updated shortly !
+
+<!--
+
 <? ob_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -45,3 +49,5 @@
 </body>
 </html>
 <? ob_flush(); ?>
+
+-->
